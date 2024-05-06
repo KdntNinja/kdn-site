705ea34a3fedd854c4e568c53fe966cb757f282b
kaiden <kaidentninja@gmail.com>
2024-05-06 16:44:01 +0100

    Functions Update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,26 +1,26 @@
-import * as admin from 'firebase-admin';
-import * as functions from 'firebase-functions';
+import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
 
 admin.initializeApp();
 
 exports.sendNotification = functions.firestore
-    .document('groups/{groupId}/posts/{postId}')
+    .document("groups/{groupId}/posts/{postId}")
     .onCreate(async (snap: functions.firestore.DocumentSnapshot, context: functions.EventContext) => {
         const post = snap.data();
         if (!post) {
-            console.error('Post data is undefined');
+            console.error("Post data is undefined");
             return;
         }
         const groupId = context.params.groupId;
 
-        const usersSnapshot = await admin.firestore().collection('users').where('group', '==', groupId).get();
+        const usersSnapshot = await admin.firestore().collection("users").where("group", "==", groupId).get();
 
         const payload = {
             notification: {
                 title: `New post by ${post.userName}`,
                 body: post.title,
                 icon: post.imageUrl,
-            }
+            },
         };
 
         usersSnapshot.forEach((doc: admin.firestore.QueryDocumentSnapshot) => {
