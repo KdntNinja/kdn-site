9378ed65bb8bac19eea4293b4fa4de3b71ca44e0
KdntNinja <kaidentninja@gmail.com>
2024-06-06 15:52:24 +0100

    Settings page initial commit

--- src/lib/registry/new-york/example/sonner-demo.svelte
+++ src/lib/registry/new-york/example/sonner-demo.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-	import { toast } from "svelte-sonner";
-	import { Button } from "$lib/registry/new-york/ui/button/index.js";
+    import { toast } from "svelte-sonner";
+    import { Button } from "$lib/registry/new-york/ui/button/index.js";
 </script>
 
 <Button
-	variant="outline"
-	on:click={() =>
-		toast.success("Event has been created", {
-			description: "Sunday, December 03, 2023 at 9:00 AM",
-			action: {
-				label: "Undo",
-				onClick: () => console.info("Undo"),
-			},
-		})}
+    variant="outline"
+    on:click="{() =>
+        toast.success('Event has been created', {
+            description: 'Sunday, December 03, 2023 at 9:00 AM',
+            action: {
+                label: 'Undo',
+                onClick: () => console.info('Undo'),
+            },
+        })}"
 >
-	Show Toast
+    Show Toast
 </Button>
