166ffeed7c0dd911f790d79dad0faee46a29a997
KdntNinja <kaidentninja@gmail.com>
2024-06-08 18:49:27 +0100

    Login Page

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -24,7 +24,7 @@
     let password = "";
 
     function isMobileDevice() {
-        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+        return /Android|webOS|iPhone|iPad/i.test(navigator.userAgent);
     }
 
     onMount(() => {
