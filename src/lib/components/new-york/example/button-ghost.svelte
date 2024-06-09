11d4730ee74d60311fee24def9e6d1d487ad849e
KdntNinja <kaidentninja@gmail.com>
2024-06-09 11:58:25 +0100

    Settings Page

--- src/lib/components/new-york/example/button-ghost.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-    import { Button } from "$lib/components/new-york/ui/button/index.js";
-</script>
-
-<Button variant="ghost">Ghost</Button>
--- src/lib/components/new-york/example/textarea-demo.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-    import { Textarea } from "$lib/components/new-york/ui/textarea/index.js";
-</script>
-
-<Textarea placeholder="Type your message here." />
