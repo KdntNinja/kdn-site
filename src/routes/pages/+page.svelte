71d872c1b9719130beccf876c79b1864ce64c05d
KdntNinja <kaidentninja@gmail.com>
2024-06-09 11:35:17 +0100

    Settings Page

--- src/routes/pages/+page.svelte
+++ src/routes/pages/+page.svelte
@@ -7,7 +7,7 @@
 
     let paths = [
         { name: "Posts", path: routes.POSTS },
-        { name: "Settings (IN PROGRESS)", path: routes.SETTINGS },
+        { name: "Settings", path: routes.SETTINGS },
     ];
 
     const goToRoute = (path: string) => {
