7ca1df30a60045036a27cd4c3835ec14f896950d
KdntNinja <kaidentninja@gmail.com>
2024-06-12 20:43:47 +0100

    Android App

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,12 @@ const config: CapacitorConfig = {
     appId: "com.kdntninja.linkloop",
     appName: "linkloop",
     webDir: "dist",
+    plugins: {
+        FirebaseAuthentication: {
+            skipNativeAuth: false,
+            providers: ["google.com"]
+        }
+    }
 };
 
 export default config;
